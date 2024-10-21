bd6d47e3a7aa0e5d2db14852c9e688ad7bc839d1
HappyHcker <70624001+HappyHcker@users.noreply.github.com>
2024-10-21 16:00:25 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//Javascript for Menu
+var navLinks = document.getElementById("redirect");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
